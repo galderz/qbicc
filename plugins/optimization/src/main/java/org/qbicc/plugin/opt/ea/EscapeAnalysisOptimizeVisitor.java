8e3e2694c38809dabb218252bf97acded8150be3
Galder Zamarreño <galder@zamarreno.com>
2021-11-11 18:59:22 +0100

    Zero stack allocated objects in EA using helper

--- plugins/optimization/src/main/java/org/qbicc/plugin/opt/ea/EscapeAnalysisOptimizeVisitor.java
+++ plugins/optimization/src/main/java/org/qbicc/plugin/opt/ea/EscapeAnalysisOptimizeVisitor.java
@@ -1,9 +1,11 @@
 package org.qbicc.plugin.opt.ea;
 
+import java.util.List;
+
+import org.qbicc.context.ClassContext;
 import org.qbicc.context.CompilationContext;
 import org.qbicc.graph.BasicBlock;
 import org.qbicc.graph.BasicBlockBuilder;
-import org.qbicc.graph.MemoryAtomicityMode;
 import org.qbicc.graph.New;
 import org.qbicc.graph.Node;
 import org.qbicc.graph.NodeVisitor;
@@ -11,24 +13,42 @@ import org.qbicc.graph.Value;
 import org.qbicc.graph.ValueHandle;
 import org.qbicc.graph.literal.IntegerLiteral;
 import org.qbicc.graph.literal.LiteralFactory;
-import org.qbicc.plugin.coreclasses.CoreClasses;
 import org.qbicc.plugin.layout.Layout;
 import org.qbicc.plugin.layout.LayoutInfo;
 import org.qbicc.type.ClassObjectType;
 import org.qbicc.type.CompoundType;
-import org.qbicc.type.ObjectType;
+import org.qbicc.type.definition.DefinedTypeDefinition;
 import org.qbicc.type.definition.LoadedTypeDefinition;
-import org.qbicc.type.definition.element.FieldElement;
+import org.qbicc.type.definition.element.MethodElement;
 
 public final class EscapeAnalysisOptimizeVisitor implements NodeVisitor.Delegating<Node.Copier, Value, Node, BasicBlock, ValueHandle> {
     private final CompilationContext ctxt;
     private final NodeVisitor<Node.Copier, Value, Node, BasicBlock, ValueHandle> delegate;
-    private final CoreClasses coreClasses;
+    private final MethodElement zeroMethod;
 
     public EscapeAnalysisOptimizeVisitor(final CompilationContext ctxt, final NodeVisitor<Node.Copier, Value, Node, BasicBlock, ValueHandle> delegate) {
         this.ctxt = ctxt;
         this.delegate = delegate;
-        this.coreClasses = CoreClasses.get(ctxt);
+
+        ClassContext classContext = ctxt.getBootstrapClassContext();
+        DefinedTypeDefinition defined = classContext.findDefinedType("org/qbicc/runtime/gc/nogc/NoGcHelpers");
+        if (defined == null) {
+            throw runtimeMissing();
+        }
+        LoadedTypeDefinition loaded = defined.load();
+        int index = loaded.findMethodIndex(e -> e.getName().equals("clear"));
+        if (index == -1) {
+            throw methodMissing();
+        }
+        zeroMethod = loaded.getMethod(index);
+    }
+
+    private static IllegalStateException runtimeMissing() {
+        return new IllegalStateException("The NoGC helpers runtime classes are not present in the bootstrap class path");
+    }
+
+    private static IllegalStateException methodMissing() {
+        return new IllegalStateException("Required method is missing from the NoGC helpers");
     }
 
     public NodeVisitor<Node.Copier, Value, Node, BasicBlock, ValueHandle> getDelegateNodeVisitor() {
@@ -50,7 +70,7 @@ public final class EscapeAnalysisOptimizeVisitor implements NodeVisitor.Delegati
     private Value stackAllocate(New new_, BasicBlockBuilder bbb) {
         ClassObjectType type = new_.getClassObjectType();
 
-        // Copied from NoGcBasicBlockBuilder
+        // Copied and adjusted from NoGcBasicBlockBuilder
         Layout layout = Layout.get(ctxt);
         LayoutInfo info = layout.getInstanceLayoutInfo(type.getDefinition());
         CompoundType compoundType = info.getCompoundType();
@@ -62,30 +82,14 @@ public final class EscapeAnalysisOptimizeVisitor implements NodeVisitor.Delegati
         Value ptrVal = bbb.stackAllocate(compoundType, lf.literalOf(1), align);
 
         Value oop = bbb.valueConvert(ptrVal, type.getReference());
-        ValueHandle oopHandle = bbb.referenceHandle(oop);
 
-        // zero initialize the object's instance fields (but not the header fields that are defined in java.lang.Object)
-        LoadedTypeDefinition curClass = type.getDefinition().load();
-        while (curClass.hasSuperClass()) {
-            curClass.eachField(f -> {
-                if (!f.isStatic()) {
-                    bbb.store(bbb.instanceFieldOf(oopHandle, f), lf.zeroInitializerLiteralOfType(f.getType()), MemoryAtomicityMode.UNORDERED);
-                }
-            });
-            curClass = curClass.getSuperClass();
-        }
-
-        // now initialize the object header (aka fields of java.lang.Object)
-        initializeObjectHeader(oopHandle, layout, type.getDefinition().load().getType(), bbb);
+        // zero initialize the object's instance fields
+        MethodElement method = zeroMethod;
+        final ValueHandle zeroCall = bbb.staticMethod(method, method.getDescriptor(), method.getType());
+        final List<Value> zeroParams = List.of(bbb.bitCast(ptrVal, type.getReference()), lf.literalOf(info.getCompoundType().getSize()));
+        bbb.call(zeroCall, zeroParams);
 
         return oop;
-    }
-
-    private void initializeObjectHeader(ValueHandle oopHandle, Layout layout, ObjectType objType, BasicBlockBuilder bbb) {
-        FieldElement typeId = coreClasses.getObjectTypeIdField();
-        bbb.store(bbb.instanceFieldOf(oopHandle, typeId),  ctxt.getLiteralFactory().literalOfType(objType), MemoryAtomicityMode.NONE);
-        FieldElement nativeObjectMonitor = CoreClasses.get(ctxt).getObjectNativeObjectMonitorField();
-        bbb.store(bbb.instanceFieldOf(oopHandle, nativeObjectMonitor), ctxt.getLiteralFactory().literalOf(0L), MemoryAtomicityMode.NONE);
     }
 
 }
