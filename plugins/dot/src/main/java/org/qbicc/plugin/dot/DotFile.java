82c9af97f1c6069fae8d26321bfb29b8e77bb6a4
Galder Zamarreño <galder@zamarreno.com>
2022-07-07 11:33:55 +0200

    Tidy up dot file generation

--- plugins/dot/src/main/java/org/qbicc/plugin/dot/DotFile.java
+++ plugins/dot/src/main/java/org/qbicc/plugin/dot/DotFile.java
@@ -1,7 +1,7 @@
 package org.qbicc.plugin.dot;
 
 import java.io.IOException;
-import java.util.Collection;
+import java.io.UncheckedIOException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
@@ -14,27 +14,34 @@ final class DotFile {
     }
 
     void writeTo(Appendable out) throws IOException {
-        out.append("digraph {").append(System.lineSeparator());
-        out.append("fontname = \"Helvetica,Arial,sans-serif\"").append(System.lineSeparator());
-        out.append("node [fontname = \"Helvetica,Arial,sans-serif\"]").append(System.lineSeparator());
-        out.append("edge [fontname = \"Helvetica,Arial,sans-serif\"]").append(System.lineSeparator());
-        out.append("graph [rankdir = TB];").append(System.lineSeparator());
-        out.append("edge [splines = true];").append(System.lineSeparator());
-        out.append(System.lineSeparator());
+        out.append(
+            """
+            digraph {
+            fontname = "Helvetica,Arial,sans-serif"
+            node [fontname = "Helvetica,Arial,sans-serif"]
+            edge [fontname = "Helvetica,Arial,sans-serif"]
+            graph [rankdir = TB];
+            edge [splines = true];
+            """
+        );
 
         disassembler.getBlocks().values().stream()
             .sorted(Comparator.comparing(Disassembler.BlockInfo::id))
             .forEach(b -> writeBlock(out, b));
 
         for (Disassembler.BlockEdge blockEdge : disassembler.getBlockEdges()) {
-            out.append(String.format(
-                "b%d -> b%d [label = %s, style = %s, color = %s];"
-                , disassembler.findBlockId(blockEdge.from())
-                , disassembler.findBlockId(blockEdge.to())
-                , blockEdge.label()
-                , blockEdge.edgeType().style()
-                , blockEdge.edgeType().color()
-            )).append(System.lineSeparator());
+            out.append(
+                """
+                b%d -> b%d [label = %s, style = %s, color = %s];
+                """
+                .formatted(
+                    disassembler.findBlockId(blockEdge.from())
+                    , disassembler.findBlockId(blockEdge.to())
+                    , blockEdge.label()
+                    , blockEdge.edgeType().style()
+                    , blockEdge.edgeType().color()
+                )
+            );
         }
 
         for (Disassembler.CellEdge cellEdge : disassembler.getCellEdges()) {
@@ -50,47 +57,57 @@ final class DotFile {
             );
         }
 
-        out.append("}").append(System.lineSeparator());
+        out.append(
+            """
+            }
+            """
+        );
     }
 
     private static void writeBlock(Appendable out, Disassembler.BlockInfo block) {
         try {
-            // TODO Consider using java text blocks to make things cleaner
-            out.append(String.format("b%d [", block.id())).append(System.lineSeparator());
-            out.append("shape = plaintext").append(System.lineSeparator());
-            out.append("label = <").append(System.lineSeparator());
-            out.append("<table border=\"0\" cellborder=\"1\" cellspacing=\"0\">").append(System.lineSeparator());
+            out.append(
+                """
+                b%d [
+                shape = plaintext
+                label = <
+                <table border="0" cellborder="1" cellspacing="0">
+                """
+                .formatted(block.id())
+            );
 
             final List<String> lines = block.lines();
             for (int i = 0; i < lines.size(); i++) {
-                final String line = lines.get(i);
-
+                final String line = formatLine(lines.get(i));
                 final String lineColor = block.lineColors().get(i);
-                out.append("<tr><td align=\"text\"");
-                out.append(" port=\"").append(String.valueOf(i)).append("\"");
-                if (Objects.nonNull(lineColor)) {
-                    out.append(" bgcolor=\"").append(lineColor).append("\"");
-                }
-                out.append(">");
 
-                String escaped = line
-                    .replaceAll(">", "&gt;")
-                    .replaceAll("<", "&lt;")
-                    .replaceAll("&", "&amp;");
-                out.append(escaped);
-
-                // Add extra space for proper formatting
-                for (int j = 0; j < line.length() / 10; j++) {
-                    out.append("&nbsp;");
-                }
-                out.append("<br align=\"left\"/></td></tr>").append(System.lineSeparator());
+                out.append(
+                    """
+                    <tr><td align="text" port="%d" bgcolor="%s">%s<br align="left"/></td></tr>
+                    """
+                    .formatted(i, Objects.nonNull(lineColor) ? lineColor : "white", line)
+                );
             }
 
-            out.append("</table>").append(System.lineSeparator());
-            out.append(">").append(System.lineSeparator());
-            out.append("]").append(System.lineSeparator());
+            out.append(
+                """
+                </table>
+                >
+                ]
+                """
+            );
         } catch (IOException e) {
-            e.printStackTrace();  // TODO: Customise this generated block
+            throw new UncheckedIOException(e);
         }
+    }
+
+    private static String formatLine(String line) {
+        String escaped = line
+            .replaceAll(">", "&gt;")
+            .replaceAll("<", "&lt;")
+            .replaceAll("&", "&amp;");
+
+        // Add extra space for proper formatting
+        return escaped + "&nbsp;".repeat(line.length() / 10);
     }
 }
